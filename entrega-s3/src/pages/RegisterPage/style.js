3039f2e2dac0396144272a3c8722b5f36c8b799a
Malaiobol <wow_ju1@hotmail.com>
2022-12-02 12:45:20 -0300

    feat:fixed error messages

--- entrega-s3/src/pages/RegisterPage/style.js
+++ entrega-s3/src/pages/RegisterPage/style.js
@@ -11,10 +11,6 @@ export const StyledForm = styled.form`
   padding: 20px;
   margin-left: 40%;
 
-  div {
-    margin-bottom: 20px;
-  }
-
   div > h2 {
     margin-left: 25%;
     margin-bottom: 20px;
@@ -34,37 +30,11 @@ export const StyledForm = styled.form`
     line-height: 22px;
   }
 
-  label {
-    font-weight: 400;
-    font-size: 0.75rem;
-    padding-top: 20px;
-  }
-
-  input {
-    margin-top: 20px;
-    margin-bottom: 20px;
-    background-color: var(--colorGrey2);
-    width: 330px;
-    height: 50px;
-
-    border: solid 2px var(--colorGrey2);
-    border-radius: 4px;
-    padding: 0px 16px;
-  }
-
-  p {
+  small {
     color: var(--colorNegative);
     margin-bottom: 20px;
 
     font-size: 1rem;
-  }
-
-  input::placeholder {
-    color: var(--colorGrey1);
-
-    font-weight: 400;
-    font-size: 1rem;
-    line-height: 26px;
   }
 
   select {
